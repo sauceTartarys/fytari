bac609008fbbc55e81c3c4d4403edb6ceaa4fa57
sauceTartarys <136168621+sauceTartarys@users.noreply.github.com>
2023-10-08 18:40:29 +0300

    ygutf

--- main.py
+++ main.py
@@ -1,66 +1,135 @@
 from PyQt5.QtWidgets import *
 
 app = QApplication([])
+
+import json
+
+try:
+    with open("notes_data.json", "r", encoding="utf-8") as file:
+        notes = json.load(file)
+
+except:
+    notes = {}
+
+
+
+
 window = QWidget()
+window.resize(800, 460)
+mainline = QHBoxLayout()
+app.setStyleSheet("""
+    QWidget {
+        background-color: purple;
+    }
 
-window.resize(600,600)
+    QPushButton{
+        background-color: brown;
+    }
+    GroupBox{
+        background: rgb(177,24,24);
 
-text = QTextEdit()
+    }
+    QRadioButton{
+        background-color: red;
+    }
+    QLabel{
+        background-color: #b11818;
+        }
+""")
 
-bludfish = QLabel("список тегів")
-nener = QLabel("пошук тегів")
-textjer = QLineEdit()
+titwar1 = QPushButton('створити замітку')
+titwar2 = QPushButton('видалити замітку')
+titwar3 = QPushButton('зберегти замітку')
+titwar4 = QPushButton('додати до замітки')
+titwar5 = QPushButton('відкріпити від замітки')
+titwar6 = QPushButton('Шукати по тегу')
+text1 = QLabel('список заміток')
+text2 = QLabel('список тегів')
+pole1 = QTextEdit()
+pole2 = QListWidget()
+pole3 = QListWidget()
+pole4 = QLineEdit()
 
-titwar0 = QPushButton("")
-titwar1 = QPushButton("")
-titwar2 = QPushButton("")
-titwar3 = QPushButton("")
-titwar4 = QPushButton("")
-titwar5 = QPushButton("")
-arktika = QListWidget("")
-jegfa = QListWidget()
-
-mainLine = QHBoxLayout()
-mainLine.addWidget(text)
-line1 = QVBoxLayout()
-lines2 = QVBoxLayout()
+linepole = QVBoxLayout()
+linemenu = QVBoxLayout()
+line1 = QHBoxLayout()
 line2 = QHBoxLayout()
-line3 = QHBoxLayout()
 
-mainLine.addLayout(line1)
+mainline.addLayout(linepole)
+mainline.addLayout(linemenu)
+linepole.addWidget(pole1)
+linemenu.addWidget(text1)
+linemenu.addWidget(pole2)
+line1.addWidget(titwar1)
+line1.addWidget(titwar2)
+linemenu.addLayout(line1)
+linemenu.addWidget(titwar3)
+linemenu.addWidget(text2)
+linemenu.addWidget(pole3)
+linemenu.addWidget(pole4)
+line2.addWidget(titwar4)
+line2.addWidget(titwar5)
+linemenu.addLayout(line2)
+linemenu.addWidget(titwar6)
 
-line1.addLayout(line2)
-line1.addLayout(line3)
-
-v1 = QVBoxLayout()
-v1.addWidget(text)
-
-
-v2 = QVBoxLayout(textjer)
-v2 = QVBoxLayout()
-v2.addWidget(bludfish)
-v2.addWidget(arktika)
-
-v2.addWidget(titwar2)
-
-v2.addWidget(titwar3)
-v2.addWidget(titwar4)
-v2.addWidget(nener)
-mainLine.addLayout(v2)
-
-v2.addWidget(jegfa)
-v2.addWidget(titwar1)
-v2.addWidget(titwar0)
-v2.addWidget(titwar5)
+def add_note():
+    note_name, ok = QInputDialog.getText(window, "Додати замітку","Назва замітки")
+    if ok and note_name != "":
+        notes[note_name] = {
+            "текст": "",
+            "теги": []
+        }
+    pole2.clear()
+    pole1.clear()
+    pole2.addItems(notes)
+    with open("notes_data.json", "w", encoding="utf-8") as file:
+        json.dump(notes, file, ensure_ascii=False, indent=4)
+def show_note():
+    # отримуємо текст із замітки з виділеною назвою та відображаємо її в полі редагування
+    key = pole2.selectedItems()[0].text()
+    print(key)
+    pole1.setText(notes[key]["текст"])
+    pole3.clear()
+    pole3.addItems(notes[key]["теги"])
 
 
+pole2.itemClicked.connect(show_note)
+
+def save_note():
+    if pole2.selectedItems():
+        key = pole2.selectedItems()[0].text()
+        notes[key]["текст"] = pole1.toPlainText()
+        with open("notes_data.json", "w", encoding="utf-8") as file:
+                json.dump(notes, file, ensure_ascii=False)
+    else:
+                print("Замітка для збереження не вибрана!")
 
 
+def del_note():
+    if pole2.selectedItems():
+        key = pole2.selectedItems()[0].text()
+        notes.pop(key)
+        pole2.clear()
+        pole1.clear()
+        pole3.clear()
+        pole2.addItems(notes)
+        with open("notes_data.json", "w", encoding="utf-8") as file:
+            json.dump(notes, file, sort_keys=True, ensure_ascii=False, indent=4)
+        print(notes)
+    else:
+        print("Замітка для вилучення не обрана!")
 
 
+titwar1.clicked.connect(add_note)
 
 
+titwar3.clicked.connect(save_note)
 
-window.setLayout(mainLine)
+
+window.setLayout(mainline)
 window.show()
+print(notes)
+pole2.addItems(notes)
+
+
 app.exec()
