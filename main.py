4c9ba482ffd1b11e576159026076ae4608782825
sauceTartarys <136168621+sauceTartarys@users.noreply.github.com>
2023-10-08 16:27:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,25 @@ app = QApplication([])
 window = QWidget()
 
 window.resize(600,600)
+app.setStyleSheet("""
+    QWidget {
+        background-color: yellow;
+    }
+
+    QPushButton{
+        background-color: brown;
+    }
+    GroupBox{
+        background: rgb(177,24,24);
+
+    }
+    QRadioButton{
+        background-color: red;
+    }
+    QLabel{
+        background-color: #b11818;
+        }
+""")
 
 text = QTextEdit()
 
@@ -11,13 +30,14 @@ bludfish = QLabel("список тегів")
 nener = QLabel("пошук тегів")
 textjer = QLineEdit()
 
-titwar0 = QPushButton("")
-titwar1 = QPushButton("")
-titwar2 = QPushButton("")
-titwar3 = QPushButton("")
-titwar4 = QPushButton("")
-titwar5 = QPushButton("")
-arktika = QListWidget("")
+titwar0 = QPushButton("основа")
+titwar1 = QPushButton("оукв")
+titwar2 = QPushButton("яйко")
+titwar3 = QPushButton("пупумпум")
+titwar4 = QPushButton("пуши яйко")
+titwar5 = QPushButton("пампампам")
+
+arktika = QListWidget()
 jegfa = QListWidget()
 
 mainLine = QHBoxLayout()
@@ -41,10 +61,11 @@ v2 = QVBoxLayout()
 v2.addWidget(bludfish)
 v2.addWidget(arktika)
 
-v2.addWidget(titwar2)
-
-v2.addWidget(titwar3)
 v2.addWidget(titwar4)
+v2.addWidget(titwar2,)
+
+v2.addWidget(titwar3),
+
 v2.addWidget(nener)
 mainLine.addLayout(v2)
 
